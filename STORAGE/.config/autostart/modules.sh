9495fa738bae23f8ab6deba49c061ae47f00f9cc
ninoh-fox <ninohfox@gmail.com>
2025-04-04 20:41:15 +0200

    disabled unness fuctions for other devices

--- STORAGE/.config/autostart/modules.sh
+++ STORAGE/.config/autostart/modules.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-cp -a /storage/bin/Equalizer.sh /storage/.config/modules/
 cp -a /storage/bin/pyxel_update.sh /storage/.config/modules/
-cp -a /storage/bin/commander.sh /storage/.config/modules/
-cp -a /storage/bin/FixPixel.sh /storage/.config/modules/
+
