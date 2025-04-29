a9d8292b4c688e877dc4dd44b51ebd50d3fd7e0f
ninoh-fox <ninohfox@gmail.com>
2025-04-29 20:07:29 +0200

    set select+start for close pico8 sa for iux

--- STORAGE/pico/pico.sh
+++ STORAGE/pico/pico.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
+. /etc/profile
 
 cd $(dirname "$0")
 export HOME=/storage/pico
+set_kill set "-9 pico8_64"
   
 if [ "$1" == "/storage/roms/pico-8/Splore.png" ]; then
 
     export LD_LIBRARY_PATH="$HOME/lib1:${LD_LIBRARY_PATH}"
+	kill_sense &
     stickmod -c /storage/pico/dpadmouse.cfg &
     sleep 1
 
     /storage/roms/bios/pico-8/pico8_64 -draw_rect 0,0,640,480 -splore -root_path /storage/roms/pico-8 -joystick 0
 
     pkill -9 stickmod
+	pkill -9 kill_sense
 
 else
 
     export LD_LIBRARY_PATH="$HOME/lib2:${LD_LIBRARY_PATH}"
+	kill_sense &
     stickmod -c /storage/pico/dpadmouse.cfg &
     sleep 1
 
     /storage/roms/bios/pico-8/pico8_64 -draw_rect 0,0,640,480 -run "$1" -root_path /storage/roms/pico-8 -joystick 0
 
     pkill -9 stickmod
+	pkill -9 kill_sense
 
 fi
 
