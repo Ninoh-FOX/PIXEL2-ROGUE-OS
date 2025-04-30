aa71245a8cae4700bbbd71f805d7a7c762eb97a3
ninoh-fox <ninohfox@gmail.com>
2025-04-30 10:13:03 +0200

    fixed mouse cursor in some apps, add sdl1.2-compat

--- STORAGE/bin/overlay.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-## Overlay the /usr/bin directory.
-mkdir -p /storage/.tmp/usr-workdir
-mkdir -p /storage/bin/overlay_usr
-mkdir -p /tmp/overlay_usr
-mount -t overlay overlay -o lowerdir=/usr,upperdir=/storage/bin/overlay_usr,workdir=/storage/.tmp/usr-workdir /usr
