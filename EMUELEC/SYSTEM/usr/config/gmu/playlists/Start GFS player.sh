42bf2a0708a18e33fc67fa1055181d75710c229c
Ninoh-FOX <ninohfox@gmail.com>
2025-08-08 17:06:21 +0200

    Added PlayGFS, media player for GBA files sound

---
+++ EMUELEC/SYSTEM/usr/config/gmu/playlists/Start GFS player.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+# Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
+
+. /etc/profile
+
+set_kill set "-HUP playgsf"
+
+/usr/bin/selector_playgsf
+sync
